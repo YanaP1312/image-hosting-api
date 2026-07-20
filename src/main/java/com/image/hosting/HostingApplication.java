0b4c52b317565f597712171d817b6ce1c0195a95
Yana Pechenenko <pechenenko.ya@gmail.com>
2026-07-20 15:12:43 +0200

    add new SpringBoot project

---
+++ src/main/java/com/image/hosting/HostingApplication.java
@@ -0,0 +1,13 @@
+package com.image.hosting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/image/hosting/HostingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.image.hosting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
